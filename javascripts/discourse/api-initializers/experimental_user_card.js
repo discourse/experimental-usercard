7acf612cc0f88fe80e3de2c2d69c3c8e1a2d382c
Jordan Vidrine <jordan@jordanvidrine.com>
2022-12-15 17:35:31 -0600

    more

--- javascripts/discourse/api-initializers/experimental_user_card.js
+++ javascripts/discourse/api-initializers/experimental_user_card.js
@@ -1,4 +1,7 @@
+import { addExtraUserClasses } from "discourse/helpers/user-avatar";
 import { apiInitializer } from "discourse/lib/api";
+import { avatarImg } from "discourse/lib/utilities";
+import { get } from "@ember/object";
 import { getURLWithCDN } from "discourse-common/lib/get-url";
 import { isEmpty } from "@ember/utils";
 import { observes } from "discourse-common/utils/decorators";
@@ -7,7 +10,7 @@ export default apiInitializer("0.11.1", api => {
   api.modifyClass("component:user-card-contents", {
     classNames: "d-user-card",
     @observes("user.card_background_upload_url")
-    addBackground() {
+    addBackground(background) {
       if (!this.allowBackgrounds) {
         return;
       }
@@ -17,9 +20,12 @@ export default apiInitializer("0.11.1", api => {
         return;
       }
 
+      // TODO: Assign background color to average color of user profile image
+
       const url = this.get("user.card_background_upload_url");
       const bg = isEmpty(url) ? "" : `url(${getURLWithCDN(url)})`;
-      thisElem.style.setProperty("--user-background", bg)
+      thisElem.style.setProperty("--user-background", bg);
     }
   });
+
 });
