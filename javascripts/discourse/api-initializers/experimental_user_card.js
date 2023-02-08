04b8494b5f65fbd923a8061f76c07b269385b3c5
Jordan Vidrine <jordan@jordanvidrine.com>
2023-02-08 10:26:28 -0600

    FIX: Addresses feedback issues & errors

--- javascripts/discourse/api-initializers/experimental_user_card.js
+++ javascripts/discourse/api-initializers/experimental_user_card.js
@@ -22,10 +22,16 @@ export default apiInitializer("0.11.1", api => {
         return;
       }
 
-      // TODO: Assign background color to average color of user profile image
+      if (!this.user) {
+        return;
+      }
 
       const backgroundUrl = this.get("user.card_background_upload_url");
       const avatar_template = get(this.user, "avatar_template").replace("{size}","240");
+
+      if (!avatar_template) {
+        return;
+      }
 
       const bg = isEmpty(backgroundUrl) ? `url(${getURLWithCDN(avatar_template)})` : `url(${getURLWithCDN(backgroundUrl)})`;
       thisElem.style.setProperty("--user-background", bg);
