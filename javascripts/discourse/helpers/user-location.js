47b647142bafade0a6d8b14cb2c9c2e4b9da6435
Jordan Vidrine <jordan@jordanvidrine.com>
2023-03-03 12:21:49 -0600

    linting

--- javascripts/discourse/helpers/user-location.js
+++ javascripts/discourse/helpers/user-location.js
@@ -1,7 +1,7 @@
 import { helper } from "@ember/component/helper";
 
 function userLocation([location]) {
-  return `https://www.google.com/maps/place/${location.replace(",","+")}`;
+  return `https://www.google.com/maps/place/${location.replace(",", "+")}`;
 }
 
 export default helper(userLocation);
