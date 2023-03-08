8f194c70c8e287044d86723da764bb4c899c6ee4
Jordan Vidrine <30537603+jordanvidrine@users.noreply.github.com>
2023-03-08 13:05:25 -0600

    FEATURE: Allow choice of OpenStreetMap or GoogleMap for user location (#12)
    - Adding open street map as link option
    - refactor

--- javascripts/discourse/api-initializers/experimental_user_card.js
+++ javascripts/discourse/api-initializers/experimental_user_card.js
@@ -1,6 +1,7 @@
 import { addExtraUserClasses } from "discourse/helpers/user-avatar";
 import { apiInitializer } from "discourse/lib/api";
 import { avatarImg } from "discourse/lib/utilities";
+import discourseComputed from "discourse-common/utils/decorators";
 import { get } from "@ember/object";
 import { getURLWithCDN } from "discourse-common/lib/get-url";
 import { isEmpty } from "@ember/utils";
@@ -10,6 +11,23 @@ export default apiInitializer("0.11.1", api => {
   api.modifyClass("component:user-card-contents", {
     classNames: "d-user-card",
     pluginId: "experimental-user-card",
+
+    @discourseComputed()
+    themeSettingValid() {
+      return ["google_maps", "open_street_map"].includes(
+        settings.user_location_maps_link
+      );
+    },
+
+    @discourseComputed()
+    userLocationLink() {
+      const mapOption = settings.user_location_maps_link;
+      if (mapOption === "google_maps") {
+        return `https://www.google.com/maps/place/${encodeURIComponent(this.user.location)}`;
+      } else {
+        return `https://www.openstreetmap.org/search?query=${encodeURIComponent(this.user.location)}`;
+      }
+    },
 
     @observes("user.card_background_upload_url")
     addBackground(background) {
@@ -27,13 +45,18 @@ export default apiInitializer("0.11.1", api => {
       }
 
       const backgroundUrl = this.get("user.card_background_upload_url");
-      const avatar_template = get(this.user, "avatar_template").replace("{size}","240");
+      const avatar_template = get(this.user, "avatar_template").replace(
+        "{size}",
+        "240"
+      );
 
       if (!avatar_template) {
         return;
       }
 
-      const bg = isEmpty(backgroundUrl) ? `url(${getURLWithCDN(avatar_template)})` : `url(${getURLWithCDN(backgroundUrl)})`;
+      const bg = isEmpty(backgroundUrl)
+        ? `url(${getURLWithCDN(avatar_template)})`
+        : `url(${getURLWithCDN(backgroundUrl)})`;
       thisElem.style.setProperty("--user-background", bg);
 
       if (isEmpty(backgroundUrl) && !isEmpty(avatar_template)) {
@@ -41,5 +64,4 @@ export default apiInitializer("0.11.1", api => {
       }
     }
   });
-
 });
